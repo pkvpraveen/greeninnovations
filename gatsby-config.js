ef90a2b28df2ab1c386d364ba9c35e80e641ab2f
Praveen Kumar Venugopala Panicker <pkvpraveen@gmail.com>
2019-07-08 00:22:41 +0530

    updated header

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,9 @@
 module.exports = {
   pathPrefix: "/greeninnovations",
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Green Innovations`,
+    description: `Green Innovations`,
+    author: `@pkv`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
--- src/components/header.js
+++ src/components/header.js
@@ -3,24 +3,24 @@ import React from "react"
 import Logo from './logo'
 
 const Header = () => (
-  <header
-    className='wrapper header'
-  >
-    <div className='logo'>
-      <Link to='/'>
-        <Logo/>
-      </Link>
-    </div>
+  <header className='header'>
+    <div className='wrapper'>
+      <div className='logo'>
+        <Link to='/'>
+          <Logo/>
+        </Link>
+      </div>
 
-    <div className='caption'>
-      <p>THE ART OF SUSTAINABLE LIVING</p>
-    </div>
-    <div className='getintouch'>
-      <p style={{
-        color: '#6d92a2',
-      }}>Get in touch</p>
-      <p><a href='tel:+919446605978'>+91 9446605978</a></p>
-      <p><a href='mailto:info@greeninnovations.in'>info@greeninnovations.in</a></p>
+      <div className='caption'>
+        <p>THE ART OF SUSTAINABLE LIVING</p>
+      </div>
+      <div className='getintouch'>
+        <p style={{
+          color: '#6d92a2',
+        }}>Get in touch</p>
+        <p><a href='tel:+919446605978'>+91 9446605978</a></p>
+        <p><a href='mailto:info@greeninnovations.in'>info@greeninnovations.in</a></p>
+      </div>
     </div>
 
   </header>
